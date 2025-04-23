67ccf386fdc2bb66f67027e7d0edfa5deffa241c
Regine Kornbakk <reginekornbakk@gmail.com>
2025-04-23 15:45:40 +0200

    feat: added shadow for the background of the search fields

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
       <h1 className="absolute left-1/2 top-14 w-xs -translate-x-1/2 bg-black/50 p-3 text-center font-rancho text-5xl text-white sm:w-sm">
         Ready for your next holiday?
       </h1>
-      <div className="absolute bottom-0 left-1/2 w-full -translate-x-1/2 -translate-y-1/4 sm:w-auto md:-translate-y-1/2">
+      <div className="absolute bottom-0 left-1/2 w-full -translate-x-1/2 -translate-y-1/4 bg-black/50 sm:w-auto md:-translate-y-1/2">
         <SearchBar />
       </div>
     </div>
