58ed45321004b1e4643144d1c1e72f70ca6f04f3
Regine Kornbakk <reginekornbakk@gmail.com>
2025-04-25 15:58:54 +0200

    feat: added constant.js and headers.js

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+export const API_URL = "https://v2.api.noroff.dev";
+
+// Venues
+export const API_VENUES = `${API_URL}/holidaze/venues`;
+export const API_VENUE = (id) => `${API_URL}/holidaze/venues/${id}`;
+
+// Bookings
+export const API_BOOKINGS = `${API_URL}/holidaze/bookings`;
+export const API_BOOKING = (id) => `${API_URL}/holidaze/bookings/${id}`;
+
+// Profiles
+export const API_PROFILES = `${API_URL}/holidaze/profiles`;
+export const API_PROFILE = (name) => `${API_URL}/holidaze/profiles/${name}`;
+export const API_PROFILE_BOOKINGS = (name) =>
+  `${API_URL}/holidaze/profiles/${name}/bookings`;
+export const API_PROFILE_VENUES = (name) =>
+  `${API_URL}/holidaze/profiles/${name}/venues`;
+export const API_PROFILE_SEARCH = (query) =>
+  `${API_URL}/holidaze/profiles/search?q=${encodeURIComponent(query)}`;
+
+// Auth
+export const API_LOGIN = `${API_URL}/auth/login`;
+export const API_REGISTER = `${API_URL}/auth/register`;
