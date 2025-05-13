6ebcb1e1f7ab2572d2577e08316dd0a22307211e
Regine Kornbakk <reginekornbakk@gmail.com>
2025-05-13 14:59:23 +0200

    feat; adding js docs

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
+/**
+ * Function to make sure to protect routes that require authentication.
+ * If not logged in, the user is redirected to login.
+ *
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - The components to render if access is granted.
+ * @returns {JSX.Element} The protected route (either children or a redirect).
+ */
+
 export default function ProtectedRoute({ children }) {
   const { user, token } = useAuth();
 
