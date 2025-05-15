05152f8790b98bc4606bd08adef8d2530dd5a77b
Regine Kornbakk <reginekornbakk@gmail.com>
2025-05-15 13:37:52 +0200

    feat: updated margin

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -180,7 +180,7 @@ function Register() {
           Registered! Redirecting...
         </div>
       )}
-      <p className="my-5 text-center font-openSans text-sm text-darkGrey">
+      <p className="mb-10 mt-5 text-center font-openSans text-sm text-darkGrey">
         Already have an account?{" "}
         <Link to="/login" className="font-bold text-red">
           Log in here!
